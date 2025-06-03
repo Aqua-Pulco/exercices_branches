a39c720c1fab3b569d0098a0c8b524e3222fcac5
AquaPulco <iris.yolal@gmail.com>
2025-06-03 10:15:19 +0200

    correction v3

--- syntaxe/debug.js
+++ syntaxe/debug.js
@@ -1,19 +1,20 @@
 // Nous souhaitons faire la somme du prix de plusieurs articles
 // Prix de deux articles
-let a1 = 10
-let a2 = 20
+let article1 = 10
+let article2 = 20
 // Nous calculons le total
-let tot = a1 + a2
+let total = article1 + article2
 //console.log(tot)
 // Prix d'un troisieme article
-let a3 = 50
+let article3 = 50
 //console.log(a3)
 // Nous ajoutons a3 au total
-tot += a3
+total += article3
 
-console.log("Le prix total avant reduction est de : ", tot)
+console.log("Le prix total avant reduction est de : ", total)
 
 //Nous appliquons 20% de reduction
-let tot2 = tot - tot*0.2
+let reduction = total*0.2
+let finalPrice = total - reduction
 
-console.log("Le prix total après reduction est de : ", tot2)
+console.log("Le prix total après reduction est de : ", finalPrice)

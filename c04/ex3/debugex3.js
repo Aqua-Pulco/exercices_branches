14a48424f2f59efec96c50ef53179548dbadd6d8
AquaPulco <iris.yolal@gmail.com>
2025-06-13 17:41:31 +0200

    version initiale non debugg

---
+++ c04/ex3/debugex3.js
@@ -0,0 +1,13 @@
+sendMessage("Je te rends visite la semaine prochaine", "Marc", "Linda");
+
+function sendMessage(message, fromName, toName)
+{
+	console.log("From : " + fromName + "to : " + toName + " Message : " + message
+
+sendMessage()
+sendMessage["Super on se voit mardi !", "Linda", "Marc"]
+
+let contactName = "Jean"
+let myName = "Ada"
+let myMessage = "Je t'apprends à coder tes premières fonctions"
+sendMessage(message, fromName, toName)

40f7d90105205486776d39cd0009d4119ec778a4
AquaPulco <iris.yolal@gmail.com>
2025-06-18 16:57:02 +0200

    ex2 fait v3 for of

--- c05/ex2/script.js
+++ c05/ex2/script.js
@@ -17,29 +17,28 @@ const horoscope = [
   }
 ];
 
+
 let horoscopeElement = document.getElementById("horoscope");
-
-for (let i = 0; i<horoscope.length;i++){  
-    const paragraphe =document.createElement("p");
-    const titre2 =document.createElement("h2");
-    titre2.innerText = horoscope[i].sign;
-    horoscopeElement.appendChild(titre2);
-    paragraphe.innerText = horoscope[i].description;
-    horoscopeElement.appendChild(paragraphe);
- }
-
 console.log(horoscopeElement);
 
 
 
+for (const item of horoscope) { //Parcours chaque élément du horoscope, et à chaque tour, mets-le dans une variable temporaire que j'appelle item.
+   const paragraphe =document.createElement("p");
+   const titre2 =document.createElement("h2");
+   titre2.innerText = item.sign;
+   horoscopeElement.appendChild(titre2);
+   paragraphe.innerText = item.description;
+   horoscopeElement.appendChild(paragraphe);
+   console.log(item.sign);
+   console.log(item.description);
+}
 
-
-// for (const item of horoscope) {
-	// TODO: remplace le console.log par le code qui ajoute un article
-	// pour chaque item avec le format suivant :
-	// <article>
-	//   <h2>Sign</h2>
-	//   <p>Description</p>
-	// </article>
-// 	console.log(item.sign, item.description);
-// }
+// for (let i = 0; i<horoscope.length;i++){  
+//     const paragraphe =document.createElement("p");
+//     const titre2 =document.createElement("h2");
+//     titre2.innerText = horoscope[i].sign;
+//     horoscopeElement.appendChild(titre2);
+//     paragraphe.innerText = horoscope[i].description;
+//     horoscopeElement.appendChild(paragraphe);
+//  }

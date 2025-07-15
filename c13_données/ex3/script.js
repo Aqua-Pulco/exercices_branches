714c06430eab988491dda0224a67662a83d50d43
AquaPulco <iris.yolal@gmail.com>
2025-07-15 11:51:07 +0200

    c13 - ex3 - fait - format

--- c13_données/ex3/script.js
+++ c13_données/ex3/script.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const filepath = 'data.json';
 
-const content = fs.readFileSync(filepath, {encoding: 'utf8', flag: 'r'});
+const content = fs.readFileSync(filepath, { encoding: 'utf8', flag: 'r' });
 const data = JSON.parse(content);
 // write your code bellow
 
@@ -11,14 +11,17 @@ console.log(data[0])
 console.log(data.length)
 
 for (const element of data) {
-        if (element.ranking === 4){
-            console.log(element);;
-        }
+    if (element.ranking === 4) {
+        console.log(element);;
+    }
 }
+
+
 let ages = 0;
+
 for (const element of data) {
-ages = ages + parseInt(element.age);
+    ages = ages + parseInt(element.age);
 }
-const ageMyen = ages/data.length;
+const ageMyen = ages / data.length;
 console.log(ageMyen);
 

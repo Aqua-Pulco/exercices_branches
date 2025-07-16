773e5dc25bf0a6e52d9b9404e2b3dd0ddf509962
AquaPulco <iris.yolal@gmail.com>
2025-07-16 12:33:35 +0200

    ex2

--- fetch_async/ex1/script.js
+++ fetch_async/ex1/script.js
@@ -1,10 +1,12 @@
 
+// 
 // objectif : 
 // <h2>Traducteur·ice en Chef d'Émojis</h2>
 /* <p>Parlez-vous couramment le langage des émojis ? Nous recrutons une personne en tant que Traducteur en Chef d'Émojis pour combler le fossé de communication entre les développeurs et les personnes concernées, qu'elles soient techniques ou non. La personne doit avoir un talent pour traduire un code complexe en émojis expressifs et vice versa. Des points bonus si la personne peut décoder les messages secrets cachés dans les 💻🤔🚀.</p> */
 
+// avoir une partie qui gere l'affichage l'autre qui gere je ne sais plus quoi
 
-//https://api.allorigins.win/raw?url= 🙏
+//https://api.allorigins.win/raw?url= 🙏ou juste ecrire https:// www. lasuite
 
 const div = document.getElementById("div");
 
@@ -15,15 +17,16 @@ function nouvelleBalise(type, texte, id) {
 }
 
 async function firstFetch() {
-    const promise = await fetch('https://api.allorigins.win/raw?url=https://codepassport.dev/api/offers');
+    const promise = await fetch('https://www.codepassport.dev/api/offers');
     const data = await promise.json()
 
-    nouvelleBalise("h", data[0].titre, div); 
+    nouvelleBalise("b", data[0].titre, div); 
     nouvelleBalise("p", data[0].description, div);
-    console.log(data.titre);
+    console.log(promise);
+    
 }
 
 firstFetch();
 
-
+// si une fonction est asynchrone elle retourne une promesse alors si pon veut recup la donne : AWAIT
 
